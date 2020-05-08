08f839f5430471928f3364b7eaa5c336b6f6c435
tusharrockpg1 <tupadhyayxyz@gmail.com>
2020-05-08 22:05:25 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+
+function showNext(){
+    window.location.href = "#next"
+   document.getElementById('next-btn').style.visibility = "hidden";
+   document.getElementById('top-btn').style.visibility = "visible";
+}
+function showTop(){
+    window.location.href = "#top"
+    document.getElementById('next-btn').style.visibility = "visible";
+    document.getElementById('top-btn').style.visibility = "hidden";
+}
+
+document.addEventListener('scroll',e=>{
+    if(window.scrollY>5){
+        document.getElementById('next-btn').style.visibility = 'hidden';
+        document.getElementById('top-btn').style.visibility = 'visible';
+    }
+    if(window.scrollY<200){
+        document.getElementById('next-btn').style.visibility = 'visible';
+        document.getElementById('top-btn').style.visibility = 'hidden';
+    }
+    if(window.scrollY>=262){
+        document.getElementById('pro1').style.visibility = "visible";
+        document.getElementById('pro1').classList.add('pro1');
+    }
+    if(window.scrollY>=460){
+        document.getElementById('pro2').style.visibility = "visible";
+        document.getElementById('pro2').classList.add('pro2');
+    }
+})
