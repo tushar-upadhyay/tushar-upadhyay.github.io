c81eee33e7a96fa12088b81a892b7a5d7fe2c7e1
tusharrockpg1 <tupadhyayxyz@gmail.com>
2020-05-09 20:21:47 +0530

    added projects

--- index.js
+++ index.js
@@ -27,34 +27,40 @@ function showTop(){
     document.getElementById('next-btn').style.visibility = "visible";
     document.getElementById('top-btn').style.visibility = "hidden";
 }
-
-document.addEventListener('scroll',e=>{
-    if(window.scrollY>5){
-        document.getElementById('next-btn').style.visibility = 'hidden';
-        document.getElementById('top-btn').style.visibility = 'visible';
-    }
-    if(window.scrollY<200){
-        document.getElementById('next-btn').style.visibility = 'visible';
-        document.getElementById('top-btn').style.visibility = 'hidden';
-    }
-    if(document.getElementById('pro1').getBoundingClientRect().top<=window.innerHeight+10){
-        document.getElementById('pro1').style.visibility = "visible";
-        document.getElementById('pro1').classList.add('pro1');
+var p1 = false;
+var p2 = false;
+var p3 = false;
+var p4 = false;
+document.addEventListener('scroll',handler);
+function handler(){
+        if(window.scrollY>5){
+            document.getElementById('next-btn').style.visibility = 'hidden';
+            document.getElementById('top-btn').style.visibility = 'visible';
+        }
+        if(window.scrollY<200){
+            document.getElementById('next-btn').style.visibility = 'visible';
+            document.getElementById('top-btn').style.visibility = 'hidden';
+        }
+        if(document.getElementById('pro1').getBoundingClientRect().top<=window.innerHeight+10 && !p1){
+            document.getElementById('pro1').style.visibility = "visible";
+            document.getElementById('pro1').classList.add('pro1');
+            p1 = true;
+           
+        }
+        if(document.getElementById('pro2').getBoundingClientRect().top<=window.innerHeight+10 && !p2){
+            document.getElementById('pro2').style.visibility = "visible";
+            document.getElementById('pro2').classList.add('pro2');
+            p2 = true;
+        }
+        if(document.getElementById('pro3').getBoundingClientRect().top<=window.innerHeight+10 && !p3){
+            document.getElementById('pro3').style.visibility = "visible";
+            document.getElementById('pro3').classList.add('pro1');
+            p3 = true;
+        }
+        if(document.getElementById('pro4').getBoundingClientRect().top<=window.innerHeight+10){
+            document.getElementById('pro4').style.visibility = "visible";
+            document.getElementById('pro4').classList.add('pro2');
+            document.removeEventListener('scroll',handler);
+        }
        
     }
-    if(document.getElementById('pro2').getBoundingClientRect().top<=window.innerHeight+10){
-        document.getElementById('pro2').style.visibility = "visible";
-        document.getElementById('pro2').classList.add('pro2');
-       
-    }
-    if(document.getElementById('pro3').getBoundingClientRect().top<=window.innerHeight+10){
-        document.getElementById('pro3').style.visibility = "visible";
-        document.getElementById('pro3').classList.add('pro1');
-      
-    }
-    if(document.getElementById('pro4').getBoundingClientRect().top<=window.innerHeight+10){
-        document.getElementById('pro4').style.visibility = "visible";
-        document.getElementById('pro4').classList.add('pro2');
-       
-    }
-})
